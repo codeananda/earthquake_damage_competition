2fde7c95aaa4417a8763cd4c122c306c6bcd44aa
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-04-01 18:07:55 +0800

    Ran black code formatter on all files

--- analysis/helpers.py
+++ analysis/helpers.py
@@ -16,22 +16,22 @@ from sklearn.metrics import f1_score
 
 
 ### USE FOR LOCAL JUPYTER NOTEBOOKS ###
-DOWNLOAD_DIR = Path('../download')
-DATA_DIR = Path('../data')
-SUBMISSIONS_DIR = Path('../submissions')
-MODEL_DIR = Path('../models')
+DOWNLOAD_DIR = Path("../download")
+DATA_DIR = Path("../data")
+SUBMISSIONS_DIR = Path("../submissions")
+MODEL_DIR = Path("../models")
 #######################################
 
 # WHAT NEEDS TO BE RETURNED?
 def preprocess_data_lgbm():
     # Load in datasets
-    X = pd.read_csv(DOWNLOAD_DIR / 'train_values.csv', index_col='building_id')
-    X_test = pd.read_csv(DOWNLOAD_DIR / 'test_values.csv', index_col='building_id')
-    y = pd.read_csv(DOWNLOAD_DIR / 'train_labels.csv', index_col='building_id')
+    X = pd.read_csv(DOWNLOAD_DIR / "train_values.csv", index_col="building_id")
+    X_test = pd.read_csv(DOWNLOAD_DIR / "test_values.csv", index_col="building_id")
+    y = pd.read_csv(DOWNLOAD_DIR / "train_labels.csv", index_col="building_id")
     # Define categorical columns
-    cat_cols = list(X.select_dtypes(include='object').columns)
-    geo_cols = ['geo_level_1_id', 'geo_level_2_id', 'geo_level_3_id']
-    bool_cols = [col for col in X.columns if col.startswith('has')]
+    cat_cols = list(X.select_dtypes(include="object").columns)
+    geo_cols = ["geo_level_1_id", "geo_level_2_id", "geo_level_3_id"]
+    bool_cols = [col for col in X.columns if col.startswith("has")]
     cat_cols.extend(geo_cols)
     cat_cols.extend(bool_cols)
     # Define non-categorical columns
@@ -40,25 +40,25 @@ def preprocess_data_lgbm():
     feature_names = cat_cols + non_cat_cols
 
     # Ordinal encode categorical vars
-    t = [('ord_encoder', OrdinalEncoder(dtype=int), cat_cols)]
-    ct = ColumnTransformer(transformers=t, remainder='passthrough')
+    t = [("ord_encoder", OrdinalEncoder(dtype=int), cat_cols)]
+    ct = ColumnTransformer(transformers=t, remainder="passthrough")
     X_all_ints = ct.fit_transform(X)
     # Label encode y
     label_enc = LabelEncoder()
     y = label_enc.fit_transform(np.ravel(y))
-    
+
     return X_all_ints, y, cat_cols, feature_names
 
 
 # Taken from the docs for lgb.train and lgb.cv
-# Helpful Stackoverflow answer: 
+# Helpful Stackoverflow answer:
 # https://stackoverflow.com/questions/50931168/f1-score-metric-in-lightgbm
 def get_ith_pred(preds, i, num_data, num_class):
     """
     preds: 1D NumPY array
         A 1D numpy array containing predicted probabilities. Has shape
-        (num_data * num_class,). So, For binary classification with 
-        100 rows of data in your training set, preds is shape (200,), 
+        (num_data * num_class,). So, For binary classification with
+        100 rows of data in your training set, preds is shape (200,),
         i.e. (100 * 2,).
     i: int
         The row/sample in your training data you wish to calculate
@@ -68,64 +68,69 @@ def get_ith_pred(preds, i, num_data, num_class):
     num_class: int
         The number of classes in your classification task.
         Must be greater than 2.
-    
-    
-    LightGBM docs tell us that to get the probability of class 0 for 
+
+
+    LightGBM docs tell us that to get the probability of class 0 for
     the 5th row of the dataset we do preds[0 * num_data + 5].
     For class 1 prediction of 7th row, do preds[1 * num_data + 7].
-    
+
     sklearn's f1_score(y_true, y_pred) expects y_pred to be of the form
     [0, 1, 1, 1, 1, 0...] and not probabilities.
-    
-    This function translates preds into the form sklearn's f1_score 
+
+    This function translates preds into the form sklearn's f1_score
     understands.
     """
     # Does not work for binary classification, preds has a different form
     # in that case
     assert num_class > 2
-    
-    preds_for_ith_row = [preds[class_label * num_data + i]
-                        for class_label in range(num_class)]
-    
+
+    preds_for_ith_row = [
+        preds[class_label * num_data + i] for class_label in range(num_class)
+    ]
+
     # The element with the highest probability is predicted
     return np.argmax(preds_for_ith_row)
-    
+
+
 def lgb_f1_micro(preds, train_data):
     y_true = train_data.get_label()
-    
+
     num_data = len(y_true)
     num_class = 3
-    
+
     y_pred = []
     for i in range(num_data):
         ith_pred = get_ith_pred(preds, i, num_data, num_class)
         y_pred.append(ith_pred)
-    
-    return 'f1', f1_score(y_true, y_pred, average='micro'), True
+
+    return "f1", f1_score(y_true, y_pred, average="micro"), True
 
 
 def make_submission_top_14_features(pipeline, title):
     """
-    Given a trained pipeline object, use it to make predictions on the 
+    Given a trained pipeline object, use it to make predictions on the
     submission test set 'test_values.csv' and write them a csv in the submissions
     folder.
     """
     # Read in test_values csv and apply data preprocessing
     # note: will create a data preprocessing pipeline or function in future
-    test_values = pd.read_csv(DATA_DIR / 'test_values.csv', index_col='building_id')
-    test_values[cat_cols] = test_values[cat_cols].astype('category')
-    test_values[bool_cols] = test_values[bool_cols].astype('bool')
+    test_values = pd.read_csv(DATA_DIR / "test_values.csv", index_col="building_id")
+    test_values[cat_cols] = test_values[cat_cols].astype("category")
+    test_values[bool_cols] = test_values[bool_cols].astype("bool")
     test_values = pd.get_dummies(test_values)
     test_values = test_values[top_14_features]
 
     # Generate predictions using pipeline we pass in
     predictions = pipeline.predict(test_values)
 
-    submission_format = pd.read_csv(DATA_DIR / 'submission_format.csv',
-                                    index_col='building_id')
+    submission_format = pd.read_csv(
+        DATA_DIR / "submission_format.csv", index_col="building_id"
+    )
 
-    my_submission = pd.DataFrame(data=predictions,
-                                columns=submission_format.columns,
-                                index=submission_format.index)
-    
-    my_submission.to_csv(SUBMISSIONS_DIR / f'{title}.csv')
+    my_submission = pd.DataFrame(
+        data=predictions,
+        columns=submission_format.columns,
+        index=submission_format.index,
+    )
+
+    my_submission.to_csv(SUBMISSIONS_DIR / f"{title}.csv")
--- notes/current_best_scores.py
+++ notes/current_best_scores.py
@@ -9,9 +9,9 @@ lgbm_02_02 submission score:     0.7407
 lgbm_02_08 val score       :     0.7849279166234973
 lgbm_02_08 submission score:     0.7335 
 """
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.33,
-                                                  random_state=42, stratify=y,
-                                                  shuffle=True)
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.33, random_state=42, stratify=y, shuffle=True
+)
 
 """
 TRAIN ON X, EVALUATE ON X_VAL (using top_14_features)
@@ -25,13 +25,20 @@ lgbm_02_08 val score (on X_val): 0.785823090966174
 lgbm_02_08 submission score:     0.7335 
 """
 
-# This only works if used with pd.get_dummies() (see last element in list)
-top_14_features = ['geo_level_1_id', 'geo_level_2_id', 'geo_level_3_id', 
-                    'count_floors_pre_eq', 'age'	, 'area_percentage'	, 
-                    'height_percentage', 
-                    'has_superstructure_mud_mortar_stone',
-                    'has_superstructure_stone_flag', 
-                    'has_superstructure_mud_mortar_brick',
-                    'has_superstructure_cement_mortar_brick',
-                    'has_superstructure_timber', 'count_families',
-                    'other_floor_type_q']
+# This only works if used with pd.get_dummies() (see last element in list)
+top_14_features = [
+    "geo_level_1_id",
+    "geo_level_2_id",
+    "geo_level_3_id",
+    "count_floors_pre_eq",
+    "age",
+    "area_percentage",
+    "height_percentage",
+    "has_superstructure_mud_mortar_stone",
+    "has_superstructure_stone_flag",
+    "has_superstructure_mud_mortar_brick",
+    "has_superstructure_cement_mortar_brick",
+    "has_superstructure_timber",
+    "count_families",
+    "other_floor_type_q",
+]
